bcc49bb3af08d625db60e1f1d1673b25759ced2b
kjacobson <kjacobson@KJACOBSON-LT2>
2022-05-13 09:17:20 -0400

    Add project files.

---
+++ InventorFlatExport/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace InventorFlatExport
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonRefreshViwer = new System.Windows.Forms.Button();
+            this.partNameLabel = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.machineToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.testConnectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.connectionSettingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonExportDxf = new System.Windows.Forms.Button();
+            this.labelPartOccur = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelPartPath = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonRefreshViwer
+            // 
+            this.buttonRefreshViwer.Location = new System.Drawing.Point(12, 376);
+            this.buttonRefreshViwer.Name = "buttonRefreshViwer";
+            this.buttonRefreshViwer.Size = new System.Drawing.Size(97, 23);
+            this.buttonRefreshViwer.TabIndex = 0;
+            this.buttonRefreshViwer.Text = "Refresh View";
+            this.buttonRefreshViwer.UseVisualStyleBackColor = true;
+            this.buttonRefreshViwer.Click += new System.EventHandler(this.buttonRefreshView_Click);
+            // 
+            // partNameLabel
+            // 
+            this.partNameLabel.Location = new System.Drawing.Point(76, 59);
+            this.partNameLabel.MinimumSize = new System.Drawing.Size(250, 0);
+            this.partNameLabel.Name = "partNameLabel";
+            this.partNameLabel.Size = new System.Drawing.Size(250, 13);
+            this.partNameLabel.TabIndex = 2;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.settingsToolStripMenuItem,
+            this.machineToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(724, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // machineToolStripMenuItem
+            // 
+            this.machineToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.testConnectionToolStripMenuItem,
+            this.connectionSettingsToolStripMenuItem});
+            this.machineToolStripMenuItem.Name = "machineToolStripMenuItem";
+            this.machineToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.machineToolStripMenuItem.Text = "Machine";
+            // 
+            // testConnectionToolStripMenuItem
+            // 
+            this.testConnectionToolStripMenuItem.Name = "testConnectionToolStripMenuItem";
+            this.testConnectionToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.testConnectionToolStripMenuItem.Text = "Test Connection";
+            // 
+            // connectionSettingsToolStripMenuItem
+            // 
+            this.connectionSettingsToolStripMenuItem.Name = "connectionSettingsToolStripMenuItem";
+            this.connectionSettingsToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.connectionSettingsToolStripMenuItem.Text = "Connection Settings";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(115, 376);
+            this.progressBar1.MarqueeAnimationSpeed = 10;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(177, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 44);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(280, 326);
+            this.treeView1.TabIndex = 5;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonExportDxf);
+            this.groupBox1.Controls.Add(this.labelPartOccur);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.labelPartPath);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(298, 33);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(414, 100);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Part Data";
+            // 
+            // buttonExportDxf
+            // 
+            this.buttonExportDxf.Enabled = false;
+            this.buttonExportDxf.Location = new System.Drawing.Point(333, 71);
+            this.buttonExportDxf.Name = "buttonExportDxf";
+            this.buttonExportDxf.Size = new System.Drawing.Size(75, 23);
+            this.buttonExportDxf.TabIndex = 5;
+            this.buttonExportDxf.Text = "Export DXF";
+            this.buttonExportDxf.UseVisualStyleBackColor = true;
+            this.buttonExportDxf.Click += new System.EventHandler(this.buttonExportDxf_Click);
+            // 
+            // labelPartOccur
+            // 
+            this.labelPartOccur.AutoSize = true;
+            this.labelPartOccur.Location = new System.Drawing.Point(107, 33);
+            this.labelPartOccur.Name = "labelPartOccur";
+            this.labelPartOccur.Size = new System.Drawing.Size(35, 13);
+            this.labelPartOccur.TabIndex = 4;
+            this.labelPartOccur.Text = "label5";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Part Occurrences:";
+            // 
+            // labelPartPath
+            // 
+            this.labelPartPath.AutoSize = true;
+            this.labelPartPath.Location = new System.Drawing.Point(107, 16);
+            this.labelPartPath.Name = "labelPartPath";
+            this.labelPartPath.Size = new System.Drawing.Size(35, 13);
+            this.labelPartPath.TabIndex = 2;
+            this.labelPartPath.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Part Path:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Inventor Parts";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 415);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.partNameLabel);
+            this.Controls.Add(this.buttonRefreshViwer);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Inventor DXF Exporter";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRefreshViwer;
+        private System.Windows.Forms.Label partNameLabel;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem machineToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem testConnectionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem connectionSettingsToolStripMenuItem;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelPartPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelPartOccur;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonExportDxf;
+    }
+}
+
---
+++ InventorFlatExport/Form1.cs
@@ -0,0 +1,362 @@
+﻿// MetalForming Inc.
+// Copyright (c) 2022 All Rights Reserved
+// Author: Kurt Jacobson
+// Date: 04/28/2022
+
+
+using System;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using Inventor;
+using IxMilia;
+using IxMilia.Dxf;
+using IxMilia.Dxf.Entities;
+
+
+
+namespace InventorFlatExport
+{
+    public partial class Form1 : Form
+    {
+
+        Inventor.Application inventor = null;
+        IDictionary<string, SheetmetalPart> partDict = new Dictionary<string, SheetmetalPart>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.labelPartPath.Text = "No Part Selected";
+            this.labelPartOccur.Text = "0";
+            this.buttonExportDxf.Enabled = false;
+        }
+
+        private void buttonRefreshView_Click(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+            InitInventor(btn);
+        }
+
+        private void InitInventor(Button button)
+        {
+
+            PartDocument oDoc = null;
+
+            try
+            {
+                inventor = (Inventor.Application)System.Runtime.InteropServices.Marshal.GetActiveObject("Inventor.Application");
+                
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Make sure AutoDesk Inventor is installed and running.", "Inventor Error!");
+                return;
+            }
+
+            
+            PopulateTreeView();
+
+            //// export in new thread
+            //oDoc = (PartDocument)inventor.ActiveDocument;
+            //new Thread(() =>
+            //{
+            //    Thread.CurrentThread.IsBackground = true;
+            //    ExportFlatDXF(oDoc);
+            //}).Start();
+
+        }
+
+
+
+        private void PopulateTreeView()
+        {
+            if (inventor == null) return;
+
+            Document actDoc = inventor.ActiveDocument;
+
+            treeView1.BeginUpdate();
+            treeView1.Nodes.Clear();
+
+            string partId = null;
+
+            // active document is a inventor part
+            if (actDoc.DocumentType == DocumentTypeEnum.kPartDocumentObject)
+            {
+                PartDocument doc = (PartDocument)actDoc;
+                partId = actDoc.InternalName;
+                TreeNode newNode = treeView1.Nodes.Add(doc.DisplayName);
+                newNode.Name = partId;
+                partDict[partId] = new SheetmetalPart(doc);
+            }
+
+
+            // active document is an inventor assembly
+            if (actDoc.DocumentType == DocumentTypeEnum.kAssemblyDocumentObject)
+            {
+                AssemblyDocument asmDoc = (AssemblyDocument)actDoc;
+                AssemblyComponentDefinition asmDeff = asmDoc.ComponentDefinition;
+
+
+                ComponentOccurrencesEnumerator leafOcc = asmDeff.Occurrences.AllLeafOccurrences;
+
+                TreeNode node = treeView1.Nodes.Add(asmDoc.DisplayName);
+
+
+                foreach (ComponentOccurrence occ in leafOcc)
+                {
+                    PartDocument doc = occ.Definition.Document;
+
+                    partId = doc.InternalName;
+
+                    if (partDict.ContainsKey(partId))
+                    {
+                        partDict[partId].PartOccurences++;
+                    }
+                    else
+                    {
+                        TreeNode newNode = node.Nodes.Add(doc.DisplayName);
+                        newNode.Name = partId;
+                        partDict[partId] = new SheetmetalPart(doc);
+                    }
+                }
+
+            }
+
+            treeView1.EndUpdate();
+            treeView1.ExpandAll();
+
+        }
+
+
+
+        private void ExportFlatDXF(PartDocument oDoc)
+
+        {
+
+            if (oDoc == null)
+            {
+                MessageBox.Show("Part document is null.", "Export error!");
+            }
+
+            if (oDoc.DocumentType == DocumentTypeEnum.kPartDocumentObject)
+            {
+                Console.WriteLine("Is Inventor Part document");
+
+                if (oDoc.SubType == "{9C464203-9BAE-11D3-8BAD-0060B0CE6BB4}");
+                {
+                    Console.WriteLine("Is sheet metal component!");
+                }
+
+                // we MUST class as a sheet metal comp def
+                SheetMetalComponentDefinition smCompDef = (SheetMetalComponentDefinition)oDoc.ComponentDefinition;
+
+                // Unfold if we don't already have a flat pattern
+                if (!smCompDef.HasFlatPattern)
+                {
+                    smCompDef.Unfold();
+                }
+
+                // set export length units
+                UnitsOfMeasure originalUnits = oDoc.UnitsOfMeasure;
+                oDoc.UnitsOfMeasure.LengthUnits = UnitsTypeEnum.kInchLengthUnits;
+
+                FlatPattern fPatt = smCompDef.FlatPattern;
+                //fPatt.Edit();
+
+                string sOut = "FLAT PATTERN DXF?AcadVersion=2007"
+                            // Outer Profile Layer
+                            //+ "&OuterProfileLayer=OUTER_PROFILE"
+                            + "&OuterProfileLayer=Outline"
+                            + "&OuterProfileLayerColor=0;0;0"
+                            + "&OuterProfileLineType=" + ((decimal)LineTypeEnum.kDefaultLineType)
+
+                            // Interior Profile Layer
+                            //+ "&InteriorProfilesLayer=INNER_PROFILES"
+                            + "&InteriorProfilesLayer=InnerOutlines"
+                            + "&InteriorProfilesLayerColor=0;0;0"
+                            + "&InteriorProfilesLineType=" + ((decimal)LineTypeEnum.kDefaultLineType)
+
+                            // Bend Up Layer
+                            //+ "&BendUpLayer=BENDLINES_UP"
+                            + "&BendUpLayer=BendingLines"
+                            + "&BendUpLayerColor=0;0;255"
+                            + "&BendUpLineType=" + ((decimal)LineTypeEnum.kDashedLineType)
+
+                            // Bend Down Layer
+                            //+ "&BendDownLayer=BENDLINES_DOWN"
+                            + "&BendUpLayer=BendingLines"
+                            + "&BendDownLayerColor=255;0;191"
+                            + "&BendDownLineType=" + ((decimal)LineTypeEnum.kDashedLineType)
+
+                            // Layers to hide
+                            + "&InvisibleLayers=IV_TANGENT;IV_ARC_CENTERS;IV_BEND;IV_BEND_DOWN;IV_UNCONSUMED_SKETCHES;BENDLINES_UP;BENDLINES_DOWN"
+                            
+                            // Other Export Settings
+                            + "&MergeProfilesIntoPolyline=True"
+                            //+ "&SimplifySplines=True"
+                            //+ "&AdvancedLegacyExport=True"
+                            //+ "&SplineTolerance=0.01"
+                            ;
+
+                string dxfOutName = System.IO.Path.GetFileNameWithoutExtension(oDoc.DisplayName) + ".dxf";
+                string dxfOutDir = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Desktop);
+                string dxfOutPath = System.IO.Path.Combine(dxfOutDir, dxfOutName);
+
+                DataIO oDataIO = oDoc.ComponentDefinition.DataIO;
+                oDataIO.WriteDataToFile(sOut, dxfOutPath);
+
+                UnitsOfMeasure docUnits = oDoc.UnitsOfMeasure;
+                string uString = docUnits.GetStringFromType(docUnits.LengthUnits);
+
+
+                // read DXF and add bend lines
+                var file = DxfFile.Load(dxfOutPath);
+                file.Header.Version = DxfAcadVersion.R2007;
+                file.ApplicationIds.Add(new DxfAppId("POS3000"));
+
+                // convert from internal units (CM) to document units
+                double toDocUnits(double value) {
+                    return docUnits.ConvertUnits(value, UnitsTypeEnum.kCentimeterLengthUnits, docUnits.LengthUnits);
+                }
+
+
+                foreach (FlatBendResult oBend in fPatt.FlatBendResults) {
+
+                    // we only want bends on the top face, so continue if on bottom face
+                    if (oBend.IsOnBottomFace) continue;
+
+                    var bAngle = oBend.Angle * 180 / Math.PI;
+
+                    // down bends have a negative bend angle
+                    if (!oBend.IsDirectionUp) bAngle *= -1.0;
+
+                    var bRadius = toDocUnits(oBend.InnerRadius);
+
+
+                    Point startPoint = oBend.Edge.StartVertex.Point;
+                    Point stopPoint = oBend.Edge.StopVertex.Point;
+
+                    // bend start and end points
+                    var x1 = toDocUnits(startPoint.X);
+                    var y1 = toDocUnits(startPoint.Y);
+                    var x2 = toDocUnits(stopPoint.X);
+                    var y2 = toDocUnits(stopPoint.Y);
+
+                    // shorten bend lines
+                    var d = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+                    // new starting point
+                    var t = 0.1 / d;
+                    x1 = (1 - t) * x1 + t * x2;
+                    y1 = (1 - t) * y1 + t * y2;
+
+                    // new stopping point
+                    t = (d - 0.1) / d;
+                    x2 = (1 - t) * x1 + t * x2;
+                    y2 = (1 - t) * y1 + t * y2;
+
+                    // create new line on BENDLINES layer
+                    var bLine = new DxfLine(new DxfPoint(x1, y1, 0.0), new DxfPoint(x2, y2, 0.0)) { Layer="BENDLINES", LineTypeName="Dashed", ColorName="red"};
+
+                    //bLine.LineTypeName = "Dashed"; 
+                    //DxfLineTypeStyle.DoubleLongDash
+
+
+                    // add XData with bend info
+                    bLine.XData["POS3000"] = new DxfXDataApplicationItemCollection(
+                        new DxfXDataString(String.Format("BendAngle:{0:F3}", bAngle)),
+                        new DxfXDataString(String.Format("BendRadius:{0:F4}", bRadius)),
+                        new DxfXDataString(String.Format("MaterialThk:{0:F4}", bRadius))
+                    );
+
+                    // add bend line to DXF
+                    file.Entities.Add(bLine);
+ 
+                }
+
+                // safe DXF file
+                file.Save(dxfOutPath);
+
+                // restore original units of measure
+                oDoc.UnitsOfMeasure.LengthUnits = originalUnits.LengthUnits;
+
+            }
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (partDict.TryGetValue(e.Node.Name, out SheetmetalPart part))
+            {
+                this.labelPartPath.Text = part.FileName;
+                this.labelPartOccur.Text = part.PartOccurences.ToString();
+                this.buttonExportDxf.Enabled = true;
+            }
+            else
+            {
+                this.labelPartPath.Text = "No Part Selected";
+                this.labelPartOccur.Text = "0";
+                this.buttonExportDxf.Enabled = false;
+            }
+        }
+
+        private void buttonExportDxf_Click(object sender, EventArgs e)
+        {
+            TreeNode selectedNode = this.treeView1.SelectedNode;
+            if (selectedNode == null) return;
+
+            if (partDict.TryGetValue(selectedNode.Name, out SheetmetalPart part))
+            {
+                ExportFlatDXF(part.Document);
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // close out the form
+            this.Close();
+        }
+    }
+}
+
+
+public class SheetmetalPart
+{
+    private PartDocument doc = null;
+    private int occurances = 1;
+
+    public SheetmetalPart(PartDocument doc)
+    {
+        this.doc = doc;
+    }
+
+    public PartDocument Document
+    {
+        get { return this.doc; }
+    }
+
+    public string Name
+    {
+        get { return this.doc.DisplayName; }
+    }
+
+    public string FileName
+    {
+        get { return this.doc.FullFileName; }
+    }
+
+    public string MaterialThikness
+    {
+        get { return this.doc.FullFileName; }
+    }
+
+    public int PartOccurences
+    {
+        get { return this.occurances; }
+        set { this.occurances = value; }
+    }
+
+}
